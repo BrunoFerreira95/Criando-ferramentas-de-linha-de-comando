a347e8c8197d770d357e4475a1fb8a61223c39c5
brunoferreira95 <brunoferreirapedraca@gmail.com>
2020-03-02 13:06:33 -0300

    CREATE

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 const { deepEqual, ok } = require('assert')
 
-const dadabase = require('./database')
+const database = require('./database')
 
 const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'Speed', id: 1}
 
 describe('Suite de manipulação de Herois', () => {
 
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
+    
     it('deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
-        const [resultado] = await dadabase.listar(expected.id)
+        const [resultado] = await database.listar(expected.id)
         deepEqual(resultado, expected)
     })
-    // it('deve cadastrar um heroi, usando arquivos', async () => {
-    //     const expected = DEFAULT_ITEM_CADASTRAR
-    //     //
-    //     onkeydown(null, expected)
-    // })
+
+    it('deve cadastrar um heroi, usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(actual, expected)
+    })
 })
