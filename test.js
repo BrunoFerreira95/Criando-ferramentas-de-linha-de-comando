bdce09f10536530babdac10234428a08f2dba0d8
brunoferreira95 <brunoferreirapedraca@gmail.com>
2020-03-02 13:24:21 -0300

    DELETE

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ describe('Suite de manipulação de Herois', () => {
     before(async () => {
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
     })
-    
+
     it('deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
         const [resultado] = await database.listar(expected.id)
@@ -21,5 +21,11 @@ describe('Suite de manipulação de Herois', () => {
         const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
         const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
         deepEqual(actual, expected)
+    })
+
+    it('deve remover um heroi por id', async () => {
+        const expected = true;
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(resultado, expected)
     })
 })
